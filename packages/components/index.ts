7c908e754ccccc095134b05ae579240fbf755332
zhengxi <453027626@qq.com>
2022-09-09 21:47:59 +0800

    修改hooks组件

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -2,10 +2,12 @@
  * @Author: 郑曦
  * @Date: 2022-09-09 17:28:03
  * @LastEditors: 郑曦
- * @LastEditTime: 2022-09-09 17:36:53
+ * @LastEditTime: 2022-09-09 21:47:21
  * @FilePath: /rush-program/packages/components/index.ts
  * @Description: 描述
  */
 import {count} from '@zxrush/hooks';
 
 console.log(count);
+
+export const Load = () => {}
